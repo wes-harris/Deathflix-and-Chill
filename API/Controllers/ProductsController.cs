cda33f6c2c49d8732a9108d3fc3693cfd6890e6f
Wesley Harris <harriscaws@gmail.com>
2024-11-16 22:10:40 -0500

    Added ProductsController

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,93 @@
+using DeathflixAPI.Data;
+using DeathflixAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public object EntityFrameworkCore { get; private set; }
+
+    public ProductsController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+    {
+        return await _context.Products.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        return product;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(Product product)
+    {
+        product.CreatedAt = DateTime.UtcNow;
+        _context.Products.Add(product);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction(
+            nameof(GetProduct),
+            new { id = product.Id },
+            product
+        );
+    }
+
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateProduct(int id, Product product)
+    {
+        if (id != product.Id)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(product).State = EntityState.Modified;
+        _context.Entry(product).Property(x => x.CreatedAt).IsModified = false;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!await _context.Products.AnyAsync(e => e.Id == id))
+            {
+                return NotFound();
+            }
+            throw;
+        }
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteProduct(int id)
+    {
+        var product = await _context.Products.FindAsync(id);
+        if (product == null)
+        {
+            return NotFound();
+        }
+
+        _context.Products.Remove(product);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+}
--- API/Models/Product.cs
+++ API/Models/Product.cs
@@ -1,8 +1,10 @@
+
 namespace DeathflixAPI.Models
 {
     public class Product
     {
         public int Id { get; set; }
         public required string Name { get; set; }
+        public DateTime CreatedAt { get; internal set; }
     }
 }
